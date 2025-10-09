b686f22ed3d2a0e5ed57e8da8a8d8d6fd969be95
mujpao <mujpao@gmail.com>
2025-10-08 23:03:07 -0700

    Handle pong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -11,8 +12,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
