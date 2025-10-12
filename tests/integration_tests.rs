deb77b7053560482b568ab31eb6e068d606a74d1
mujpao <mujpao@gmail.com>
2025-10-11 17:44:06 -0700

    Fix integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,32 +1,71 @@
 use codecrafters_redis::run;
 use tokio::net::TcpListener;
+use tokio::task::JoinHandle;
 
-#[tokio::test]
-async fn handle_pings_from_multiple_connections() {
+async fn setup() -> (u16, JoinHandle<()>) {
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let port = listener.local_addr().unwrap().port();
-    tokio::spawn(async {
+    let handle = tokio::spawn(async {
         run(listener).await.unwrap();
     });
 
-    let client1 = redis::Client::open(format!("redis://127.0.0.1:{}/", port)).unwrap();
-    let mut conn1 = client1.get_connection().unwrap();
+    (port, handle)
+}
 
-    let client2 = redis::Client::open(format!("redis://127.0.0.1:{}/", port)).unwrap();
-    let mut conn2 = client2.get_connection().unwrap();
+#[tokio::test]
+async fn handle_pings_from_multiple_connections() {
+    let (port, handle) = setup().await;
+    let connection_str = format!("redis://127.0.0.1:{}/", port);
+    let client1 = redis::Client::open(connection_str.clone()).unwrap();
+
+    let mut conn1 = client1.get_multiplexed_async_connection().await.unwrap();
+
+    let client2 = redis::Client::open(connection_str).unwrap();
+    let mut conn2 = client2.get_multiplexed_async_connection().await.unwrap();
 
     let data: String = redis::cmd("PING")
-        .query(&mut conn1)
+        .query_async(&mut conn1)
+        .await
         .expect("failed to execute PING");
     assert_eq!(data, "PONG");
 
     let data: String = redis::cmd("PING")
-        .query(&mut conn2)
+        .query_async(&mut conn2)
+        .await
         .expect("failed to execute PING");
     assert_eq!(data, "PONG");
 
     let data: String = redis::cmd("PING")
-        .query(&mut conn1)
+        .query_async(&mut conn1)
+        .await
         .expect("failed to execute PING");
     assert_eq!(data, "PONG");
+}
+
+#[tokio::test]
+async fn handle_echo() {
+    let (port, handle) = setup().await;
+
+    let client = redis::Client::open(format!("redis://127.0.0.1:{}/", port)).unwrap();
+    let mut conn = client.get_multiplexed_async_connection().await.unwrap();
+
+    let data: String = redis::cmd("ECHO")
+        .query_async(&mut conn)
+        .await
+        .expect("failed to execute ECHO");
+    assert_eq!(data, "");
+
+    let data: String = redis::cmd("echo")
+        .arg("hello world")
+        .query_async(&mut conn)
+        .await
+        .expect("failed to execute ECHO");
+    assert_eq!(data, "hello world");
+
+    let data: String = redis::cmd("eCho")
+        .arg("here is some data")
+        .query_async(&mut conn)
+        .await
+        .expect("failed to execute ECHO");
+    assert_eq!(data, "here is some data");
 }
